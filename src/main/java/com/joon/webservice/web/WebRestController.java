8043bceac821ef4a80e7c3abf76be64ab9575177
Joon <joonthy@gmail.com>
2019-02-20 22:23:12 +0900

    Hibernate 5.2.11로 버전 변경

--- src/main/java/com/joon/webservice/web/WebRestController.java
+++ src/main/java/com/joon/webservice/web/WebRestController.java
@@ -2,11 +2,13 @@ package com.joon.webservice.web;
 
 import com.joon.webservice.domain.PostsRepository;
 import com.joon.webservice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@AllArgsConstructor
 @RestController
 public class WebRestController {
 
