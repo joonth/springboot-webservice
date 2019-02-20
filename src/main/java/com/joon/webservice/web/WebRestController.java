1a74f7b08dbdfc063fe11c6ec8f9ff99b79f7013
Joon <joonthy@gmail.com>
2019-02-20 21:47:37 +0900

    Controller & DTO 구현

--- src/main/java/com/joon/webservice/web/WebRestController.java
+++ src/main/java/com/joon/webservice/web/WebRestController.java
@@ -1,14 +1,24 @@
 package com.joon.webservice.web;
 
+import com.joon.webservice.domain.PostsRepository;
+import com.joon.webservice.dto.posts.PostsSaveRequestDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello(){
         return "Hi";
     }
 
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
+    }
 }
