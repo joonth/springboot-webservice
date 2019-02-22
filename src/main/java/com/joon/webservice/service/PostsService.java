3c9e8ea7ad59d0f173b3ef5b31db6337213e207c
Joon <joonthy@gmail.com>
2019-02-22 21:40:33 +0900

    게시글 출력 구현

--- src/main/java/com/joon/webservice/service/PostsService.java
+++ src/main/java/com/joon/webservice/service/PostsService.java
@@ -1,11 +1,15 @@
 package com.joon.webservice.service;
 
 import com.joon.webservice.domain.PostsRepository;
+import com.joon.webservice.dto.posts.PostsMainResponseDto;
 import com.joon.webservice.dto.posts.PostsSaveRequestDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @AllArgsConstructor
 @Service
@@ -15,5 +19,12 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto dto){
         return postsRepository.save(dto.toEntity()).getId();
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsMainResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
