cd47ac0a65a1a839dc3aa8635a89ce813cd821e7
Joon <joonthy@gmail.com>
2019-03-02 20:16:54 +0900

    활성화된 profile 반환하는 code

--- src/main/java/com/joon/webservice/web/WebRestController.java
+++ src/main/java/com/joon/webservice/web/WebRestController.java
@@ -4,17 +4,21 @@ import com.joon.webservice.domain.PostsRepository;
 import com.joon.webservice.dto.posts.PostsSaveRequestDto;
 import com.joon.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
+
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 @AllArgsConstructor
 @RestController
 public class WebRestController {
 
-    private PostsRepository postsRepository;
     private PostsService postsService;
+    private Environment env;
 
     @GetMapping("/hello")
     public String hello(){
@@ -24,5 +28,12 @@ public class WebRestController {
     @PostMapping("/posts")
     public Long savePosts(@RequestBody PostsSaveRequestDto dto){
         return postsService.save(dto);
+    }
+
+    @GetMapping("/profile")
+    public String getProfile(){
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
     }
 }
